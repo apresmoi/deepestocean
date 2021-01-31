0555f0953bc8d3aa7b80110d1b9c1923cb4f1e61
lsejudo <luciano.sejudo@gmail.com>
2021-01-30 22:33:28 -0300

    handle lobby

--- client/src/components/Lobby/Lobby.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useGame } from "@store";
-import { usePlayers } from "@hooks";
-
-export function Lobby() {
-	const players = usePlayers();
-	console.log(players);
-	return <div className="lobby">lobby</div>;
-}
---
+++ client/src/views/Lobby/Lobby.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useGame } from "@store";
+import { usePlayers } from "@hooks";
+
+export function Lobby() {
+	const players = usePlayers();
+	const history = useHistory();
+	const playersNames = Object.keys(players)
+	return (
+		<div className="lobby">
+			<div>
+				{playersNames.map(player => <div>{player}</div>)}
+			</div>
+			<h3 onClick={() => history.push('/play')}>
+				Play!
+			</h3>
+		</div>
+	)
+}

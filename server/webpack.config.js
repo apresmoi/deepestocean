e9bf31523756ee255bd835a7e18c344fc77fb6dc
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2021-01-31 12:29:10 -0300

    deploy

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,33 +1,36 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const WebpackShellPlugin = require('webpack-shell-plugin');
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const WebpackShellPlugin = require("webpack-shell-plugin");
+
+const { NODE_ENV = "production" } = process.env;
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'development',
-  target: 'node',
-  watch: true,
-  externals: [nodeExternals()],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  plugins: [
-    new WebpackShellPlugin({
-      onBuildEnd: ['npm run dev']
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          'ts-loader'
-        ]
-      }
-    ]
-  }
-}
+	entry: "./src/index.ts",
+	mode: NODE_ENV,
+	target: "node",
+	watch: NODE_ENV === "development",
+	externals: [nodeExternals()],
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "index.js",
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	plugins:
+		NODE_ENV === "development"
+			? [
+					new WebpackShellPlugin({
+						onBuildEnd: ["npm run dev"],
+					}),
+			  ]
+			: [],
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: ["ts-loader"],
+			},
+		],
+	},
+};

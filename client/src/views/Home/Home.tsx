b76402457b18d0410da0c2769467dc04fe0287a4
Mariana Beldi <mariana.beldi@leniolabs.com>
2021-01-31 11:49:50 -0300

    home

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -7,18 +7,20 @@ export function Home() {
 	return (
 		<div className="home">
 			<div className="home__title">Deepest Ocean</div>
-			<div className="home__label">
-				- You'll be assigned 3 random creatures to find in the ocean
+			<div className="content-container">
+				<div className="home__label">
+					- You'll be assigned 3 random creatures to find in the ocean
+				</div>
+				<img src="/images/instructions1.svg" />
+				<div className="home__label">
+					- Choose a room to play with your friends (max. 5)
+				</div>
+				<img src="/images/instructions2.svg" height={40}/>
+				<div className="home__label">
+					- Choose what part of the ship to drive
+				</div>
+				<img src="/images/instructions3.svg" height={80}/>
 			</div>
-			<img src="/images/instructions1.svg" />
-			<div className="home__label">
-				- Choose a room to play with your friends (max. 5)
-			</div>
-			<img src="/images/instructions2.svg" height={40}/>
-			<div className="home__label">
-				- Choose what part of the ship to drive
-			</div>
-			<img src="/images/instructions3.svg" height={80}/>
 			<div className="home__button" onClick={() => history.push('/login')}>start a game!</div>
 		</div>
 	)

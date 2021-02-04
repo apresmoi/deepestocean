6b15f52cbba3b5f315bcae480b6210d1e97cccd5
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2021-02-04 11:09:51 -0300

    sound

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,25 @@ import * as React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import { Game, Home, CreateRoom, Lobby, Rooms, Login } from "./views";
-import { ConnectionStore, EventsStore } from "@store";
+import { ConnectionStore, EventsStore, SettingsStore } from "@store";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<EventsStore>
-				<ConnectionStore>
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/rooms" component={Rooms} />
-						<Route exact path="/create-room" component={CreateRoom} />
-						<Route exact path="/lobby" component={Lobby} />
-						<Route exact path="/play" component={Game} />
-						<Redirect to={"/play"} />
-					</Switch>
-				</ConnectionStore>
+				<SettingsStore>
+					<ConnectionStore>
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/login" component={Login} />
+							<Route exact path="/rooms" component={Rooms} />
+							<Route exact path="/create-room" component={CreateRoom} />
+							<Route exact path="/lobby" component={Lobby} />
+							<Route exact path="/play" component={Game} />
+							<Redirect to={"/play"} />
+						</Switch>
+					</ConnectionStore>
+				</SettingsStore>
 			</EventsStore>
 		</BrowserRouter>
 	);
